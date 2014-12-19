4f826626616115d131d3a0a71280261f91b8d9f1
jdegraad <jdegraad@gmail.com>
2014-12-19 14:04:23 -0800

    v1.0 working

---
+++ scoring-app-v1.0/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/jdegraad/development/sqtest1/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/jdegraad/development/sqtest1/platforms/android/AndroidManifest.xml \
---
+++ scoring-app-v1.0/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/jdegraad/development/sqtest1/platforms/android/ant-build/classes.dex : \
+/Users/jdegraad/development/sqtest1/platforms/android/ant-build/classes/com/example/sqtest1/BuildConfig.class \
+/Users/jdegraad/development/sqtest1/platforms/android/ant-build/classes/com/example/sqtest1/CordovaApp.class \
+/Users/jdegraad/development/sqtest1/platforms/android/ant-build/classes/com/example/sqtest1/R$attr.class \
+/Users/jdegraad/development/sqtest1/platforms/android/ant-build/classes/com/example/sqtest1/R$drawable.class \
+/Users/jdegraad/development/sqtest1/platforms/android/ant-build/classes/com/example/sqtest1/R$string.class \
+/Users/jdegraad/development/sqtest1/platforms/android/ant-build/classes/com/example/sqtest1/R$xml.class \
+/Users/jdegraad/development/sqtest1/platforms/android/ant-build/classes/com/example/sqtest1/R.class \
+/Users/jdegraad/development/sqtest1/platforms/android/ant-build/dexedLibs/classes-a4de98d5fd7d2e36218b4d74c703157f.jar \
